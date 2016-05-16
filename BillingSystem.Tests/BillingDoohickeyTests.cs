a409a2d2545edcb6150e837c6d6736a3b1fb66d4
Vladimir Šobić <vsobic@gmail.com>
2016-05-17 00:27:36 +0200

    Added first user story - CustomerWhoDoesNotHaveSubscriptionDoesNotGetCharged

---
+++ BillingSystem.Tests/BillingDoohickeyTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+
+namespace BillingSystem.Tests
+{
+    public class BillingDoohickeyTests
+    {
+        // Monthly billing
+        // Grace period for missed payments ("dunning" status)
+        // Not all customers are necessarily subscribers
+        // Idle customers should be automatically unsubscribed
+
+        [Fact]
+        public void CustomerWhoDoesNotHaveSubscriptionDoesNotGetCharged()
+        {
+            // Source of customers
+            // Service for charging customers
+            var repo = new Mock<ICustomerRepository>();
+            var charger = new Mock<ICreditCardCharger>();
+            var customer = new Customer(); // What does it mean to not have a subscription?
+            var thing = new BillingDoohickey(repo.Object,charger.Object);
+
+            thing.ProcessMonth(2011,8);
+
+            charger.Verify(c => c.ChargeCustomer(customer), Times.Never());
+        }
+        
+    }
+
+    public interface ICustomerRepository
+    {
+    }
+
+    public interface ICreditCardCharger
+    {
+        void ChargeCustomer(Customer customer);
+    }
+
+    public class Customer
+    {
+    }
+
+    public class BillingDoohickey
+    {
+        ICustomerRepository repo;
+        ICreditCardCharger charger;
+
+        public BillingDoohickey(ICustomerRepository repo, ICreditCardCharger charger)
+        {
+            this.repo = repo;
+            this.charger = charger;
+        }
+
+        public void ProcessMonth(int year, int month)
+        {
+        }
+    }
+}
